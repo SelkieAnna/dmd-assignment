b9a9b8342bbae1d4449613866dba9ecfe5fc5fcf
SelkieAnna <monstrolog@gmail.com>
2018-11-07 23:11:54 +0300

    changees

--- DMD.sql
+++ DMD.sql
@@ -59,23 +59,70 @@ CREATE TABLE IF NOT EXISTS Fixes (
   time_and_date VARCHAR(255) NOT NULL ,
   car_id        INT          NOT NULL ,
   workshop_id   INT          NOT NULL ,
-  FOREIGN KEY  (car_id) REFERENCES Car(id),
-  FOREIGN KEY  (workshop_id) REFERENCES Workshop(id)
+  FOREIGN KEY  (car_id) REFERENCES Car (registration_number),
+  FOREIGN KEY  (workshop_id) REFERENCES Workshop (id)
 );
 
 CREATE TABLE IF NOT EXISTS Has_availble (
   workshop_id   INT        NOT NULL ,
   car_part_id   INT        NOT NULL ,
   amount        INT        NOT NULL ,
-  FOREIGN KEY (workshop_id) REFERENCES Workshop(id),
-  FOREIGN KEY (car_part_id) REFERENCES Car_part(id)
+  FOREIGN KEY (workshop_id) REFERENCES Workshop (id),
+  FOREIGN KEY (car_part_id) REFERENCES Car_part (id)
 );
 
 CREATE TABLE  IF NOT EXISTS Provides(
   car_part_id   INT       NOT NULL ,
   part_drovider INT       NOT NULL ,
-  FOREIGN KEY (car_part_id) REFERENCES Car_part(id),
-  FOREIGN KEY (part_drovider) REFERENCES Part_provider(id)
+  FOREIGN KEY (car_part_id) REFERENCES Car_part (id),
+  FOREIGN KEY (part_drovider) REFERENCES Part_provider (id)
 );
+
+CREATE TABLE Customer (
+    id              INT             PRIMARY KEY,
+    residence       VARCHAR(255)    NOT NULL,
+    full_name       VARCHAR(255)    NOT NULL,
+    username        VARCHAR(255)    NOT NULL,
+    email           VARCHAR(255)    NOT NULL,
+    phone_number    VARCHAR(255)    NOT NULL
+);
+
+CREATE TABLE Car_order (
+    customer_id     INT             NOT NULL,
+    car_id          INT             NOT NULL,
+    point_a         VARCHAR(255)    NOT NULL,
+    point_b         VARCHAR(255)    NOT NULL,
+    date_time       DATETIME        NOT NULL,
+    price           DECIMAL         NOT NULL,
+
+    FOREIGN KEY (customer_id) REFERENCES Customer (id),
+    FOREIGN KEY (car_id) REFERENCES Car (registration_number)
+);
+
+CREATE TABLE Part_provider (
+    id              INT             PRIMARY KEY,
+    address         INT             NOT NULL,
+    phone_number    VARCHAR(255)    NOT NULL,
+    name            VARCHAR(255)    NOT NULL
+);
+
+CREATE TABLE Part_order (
+    provider_id     INT             NOT NULL,
+    workshop_id     INT             NOT NULL,
+    part_id         INT             NOT NULL,
+    amount          DECIMAL         NOT NULL,
+
+    FOREIGN KEY (provider_id) REFERENCES Part_provider (id),
+    FOREIGN KEY (workshop_id) REFERENCES Workshop (id),
+    FOREIGN KEY (part_id) REFERENCES Car_part (id)
+);
+
+CREATE TABLE Car_part (
+    id              INT             PRIMARY KEY,
+    color           VARCHAR(255)    NOT NULL,
+    cost            DECIMAL         NOT NULL,
+    name            INT             NOT NULL
+);
+
 
 
