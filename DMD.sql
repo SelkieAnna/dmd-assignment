d7a5c2b018ff05ae6204240e530d9e92f5d03783
John Doe <saitov66@gmail.com>
2018-11-07 21:59:26 +0300

    added socket-car relation

--- DMD.sql
+++ DMD.sql
@@ -8,8 +8,11 @@ CREATE TABLE IF NOT EXISTS Socket (
   id         INT   NOT NULL,
   voltage    FLOAT NOT NULL,
   station_id INT   NOT NULL,
+  cur_charging INT NULL,
+
   PRIMARY KEY (id),
-  FOREIGN KEY (station_id) REFERENCES Charging_station (id)
+  FOREIGN KEY (station_id) REFERENCES Charging_station (id),
+  FOREIGN KEY (cur_charging) REFERENCES Socket_car (socket_id)
 );
 
 CREATE TABLE IF NOT EXISTS Car_model (
@@ -28,12 +31,29 @@ CREATE TABLE IF NOT EXISTS Workshop (
 
 CREATE TABLE IF NOT EXISTS Car (
   registration_number INT PRIMARY KEY,
+  car_model INT NOT NULL,
   type                VARCHAR(255) NOT NULL,
   location            VARCHAR(255) NOT NULL,
   color               VARCHAR(255) NOT NULL,
   charge_level_in_MaH INT          NOT NULL,
   full_charge_in_MaH  INT          NOT NULL,
   busy                TINYINT      NOT NULL,
-  `condition`         TINYINT      NOT NULL
+  `condition`         TINYINT      NOT NULL,
+
+  current_charge INT NULL,
+
+  FOREIGN KEY (car_model) REFERENCES Car_model (id),
+  FOREIGN KEY (current_charge) REFERENCES Socket_car (car_id)
 );
+
+CREATE TABLE IF NOT EXISTS Socket_car (
+  socket_id INT NOT NULL,
+  car_id INT NOT NULL,
+  time_date DATETIME NOT NULL,
+
+  FOREIGN KEY (socket_id) REFERENCES Socket (id),
+  FOREIGN KEY (car_id) REFERENCES Car (registration_number)
+)
+
+
 
