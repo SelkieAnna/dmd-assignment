2bcbfe2c951e254548cffa5c2a39867c7550af9b
John Doe <saitov66@gmail.com>
2018-11-26 01:26:16 +0300

    added case for query 1 to db

--- Data_Fill.sql
+++ Data_Fill.sql
@@ -19,14 +19,16 @@ INSERT INTO Car (registration_number, car_model, location, color, charge_level, 
                    ('X123UY', '1', 'uni, 1', 'violet', '4000',  '0', '1', '0'),
                    ('K234EK', '2', 'uni, 2', 'sky blue', '3000',  '1', '0', '0'),
                    ('P234OH', '2', 'Kazahstan', 'space earl grey', '2000', '1', '0', '0'),
-                   ('E235BA', '3', 'uni, 2', 'skyblue', '3000', '0', '0', '0');
+                   ('E235BA', '3', 'uni, 2', 'skyblue', '3000', '0', '0', '0'),
+                   ('AN123B', '2', 'uni,2', 'red', '5000', '0', '0', '0');
 
 INSERT INTO Car_order (customer_id, car_id, point_a, point_b, date_time, price, distance, trip_duration)
             VALUES
                    ('3', 'X123UY', '225 N Willow St, Telluride, CO, 81435', '2003 Jay St, Walnut Ridge, AR, 72476', '2011-11-11 11:11:11', '400', '1.5', '25'),
                    ('1', 'P234OH', '55 Hoover Holw, Garrett, KY, 41630', '95 Royal Dr, Carson City, NV, 89706', '2018-10-11 12:15:17', '300', '0.7', '30'),
                    ('1', 'P234OH', '55 Hoover Holw, Garrett, KY, 41630', '95 Royal Dr, Carson City, NV, 89706', '2018-10-11 12:15:17', '300', '0.1', '30'),
-                   ('2', 'E235BA', '505 Center Pl, Williamsport, PA, 17701', '1541 Owenwood Cir, Sandy, UT, 84092', '2018-10-05 15:54:56', '500', '3', '17.5');
+                   ('2', 'E235BA', '505 Center Pl, Williamsport, PA, 17701', '1541 Owenwood Cir, Sandy, UT, 84092', '2018-10-05 15:54:56', '500', '3', '17.5'),
+                   ('2', 'AN123B', '225 N Willow St, Telluride, CO, 81435', '2003 Jay St, Walnut Ridge, AR, 72476', '2018-10-04 12:15:17', '500', '4', '20.5');
 
 INSERT INTO Workshop(location)
             VALUES ('Califirnication, 3'),
